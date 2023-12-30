0b9bfb86ccc4080c77905e78b10ed893a6b56a19
venkatramankannantech <venkatramankannantech@gmail.com>
2023-12-30 13:42:53 +0530

    introduce redundancy

--- transaction.py
+++ transaction.py
@@ -5,6 +5,7 @@ from account import Account
 def perform_transaction(account, transaction_type, amount):
     if transaction_type == "deposit":
         account.deposit(amount)
+        print('amount deposited')
     elif transaction_type == "withdraw":
         account.withdraw(amount)
     else:
