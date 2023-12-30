70122f0f4e88fff5ee1d1a536055a924052d2f04
Venkatraman Kannan <venkatraman@appsforbharat.com>
2023-12-30 13:16:55 +0530

    minor print statement fix

--- transaction.py
+++ transaction.py
@@ -1,10 +1,11 @@
 # transaction.py
 from account import Account
 
+
 def perform_transaction(account, transaction_type, amount):
     if transaction_type == "deposit":
         account.deposit(amount)
     elif transaction_type == "withdraw":
         account.withdraw(amount)
     else:
-        print("Invalid transaction type")
+        print("Please enter a valid transaction type")
